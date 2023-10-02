8c06fdbb05d0554f33bb821f6b693ac2311fb585
rutvik8111999 <rutvik8111999403@gmail.com>
2023-10-02 18:48:47 +0530

    [RORT-1] Initial Commit

---
+++ scope.rb
@@ -0,0 +1,13 @@
+a = 5
+
+3.times do |n|    
+  a = 3
+  b = 5          
+end
+puts a
+puts b    
+
+# MY_CONSTANT = 'I am available throughout your app.'  constant variable
+
+# Example of a global variable declaration:
+# $var = 'I am also available throughout your app.'
