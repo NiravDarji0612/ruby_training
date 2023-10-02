b10c18ce382441044fdfdb4d9446c8b2427bb055
sonipratham97 <sonipratham64@gmail.com>
2023-10-02 18:19:31 +0530

    RORT-21 implementing conditions,operators,user input,method

---
+++ logicaloperators.rb
@@ -0,0 +1,6 @@
+a = 50
+b = 100
+
+puts "a || b is #{a || b}"
+puts "a && b is #{a && b}"
+puts "a & b is #{a & b}"
---
+++ mehod.rb
@@ -0,0 +1,7 @@
+#method
+def add(a,b)
+    c = a+b
+    puts c
+end
+add(1,2)
+add(5,6)
