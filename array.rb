d9123ec8c9ed045dd011e9f90656635348151760
sonipratham97 <sonipratham64@gmail.com>
2023-10-03 16:36:54 +0530

    RORT-18 intro of array functions

--- array.rb
+++ array.rb
@@ -2,29 +2,24 @@ array = Array.new(25)
 puts "array length is #{array.length}"
 puts "array sige is #{array.size}"
 
-#next
 array = Array.new(4,"pratham")
 puts array
 
-#next
 digits = [1,5]
 puts digits.include?(5)
 ret = digits.min
 puts "Min value is #{ret}"
 
-#next
 digits.each do |e|
-    puts "all digit is #{e}"
+puts "all digit is #{e}"
 end
-#next
+
 digits = Array(0..9)
 puts digits
-#next 
+
 digits = Array(0..9)
 num = digits.at(6)
 puts "#{num}"
-
-#next 
 
 array = [1,2,3,4,5,6,7,8,9]
 puts "the firsh number is:#{array.first}"
@@ -41,12 +36,3 @@ puts "use include method is #{array1.include?(10)}"
 
 array3= [1,[1,2],[1,2,3]]
 puts "use flatten method is #{array3.flatten}"
-# puts "use each with index method is #{array3.each_with_index}"
-
-
-
-
-
-
-
-
